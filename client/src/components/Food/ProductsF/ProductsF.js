20cd1975bbd0dee312fa38235fa96fde7f44d146
chevtong <chevtongwingyan@gmail.com>
2021-03-20 00:46:44 +0100

    Add products page, shopping cart state, localStorage for shopping cart

--- client/src/components/Food/ProductsF/ProductsF.js
+++ client/src/components/Food/ProductsF/ProductsF.js
@@ -1,11 +1,73 @@
-import React from 'react'
+import React,{useEffect} from "react";
+import "./ProductsF.css";
+import { FaLeaf } from "react-icons/fa";
+import { IoMdArrowBack } from "react-icons/io";
+import StarIcon from "@material-ui/icons/Star";
+import EuroSymbolIcon from "@material-ui/icons/EuroSymbol";
 
-function ProductsF() {
-    return (
-        <div>
-            Product details
-        </div>
-    )
+import { Link } from "react-router-dom";
+
+function ProductsF({ selectedDish, setSelectedDish,shoppingCart, setShoppingCart }) {
+
+   const addCart = () => {
+
+    setShoppingCart([...shoppingCart, {
+        dish: selectedDish.dishName,
+        cooker: selectedDish.cooker,
+        price: selectedDish.price,
+        pickUpDate: selectedDish.pickUpDate,
+        address: selectedDish.address
+    }])
+   }
+
+  
+
+  return (
+    <div>
+        <div className="products-wrapper">
+
+      <Link to="/food">
+        <button onClick={() => setSelectedDish([])}>
+          <IoMdArrowBack className="back-button" />
+        </button>
+      </Link>
+
+      {typeof selectedDish.dishName != "undefined" ? (
+          <div>
+          <div className="products-info">
+            <img src={selectedDish.imageUrl} alt="" />
+            {selectedDish.ingredients.map((ingredient) => (
+              <span key={ingredient} className="ingredient-lable">
+                {ingredient}
+              </span>
+            ))}
+            {selectedDish.vegetarian ? <FaLeaf /> : ""}
+            <h3>{selectedDish.dishName}</h3>
+            <p>{selectedDish.dishDescription}</p>
+            <p>
+              {selectedDish.price} <EuroSymbolIcon />
+            </p>
+          </div>
+
+          <div className="cooker-info">
+            <img src={selectedDish.cookerImage} alt="" />
+            {selectedDish.cooker}
+            {selectedDish.cookerScore.map((score, index) => (
+              <StarIcon key={index} className="star" />
+            ))}
+          </div>
+
+          <button onClick={addCart}>Add to cart</button>
+          </div>
+
+      ) : (
+        <h1>Go back to check out the delicious HOME DISHES</h1>
+      )}
+
+    </div>
+    </div>
+
+  );
 }
 
-export default ProductsF
+export default ProductsF;
--- client/src/components/Food/Template.js
+++ client/src/components/Food/Template.js
@@ -1,7 +1,12 @@
 export const FoodTable = [
   {
+    dishID: "1",
     dishName: "Chicken Satay",
+    ingredients: ["Chicken", "peanut", "coconut milk", "chili"],
+    vegetarian: 0,
+    dishDescription: "Delicious home made chicken satay in Malaysian style",
     cooker: "Aluka",
+    cookerImage:"https://images.unsplash.com/photo-1577219491135-ce391730fb2c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8Y2hlZnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
     cookerScore: ["star"],
     price: "10",
     pickupDate: "30_03_2020",
@@ -10,8 +15,13 @@ export const FoodTable = [
       "https://i.pinimg.com/originals/2c/f9/7a/2cf97a3e507f406a700445e56e5a9507.jpg",
   },
   {
+    dishID: "2",
     dishName: "beef bugolgi",
+    ingredients: ["beef", "rice", "spring onion", "seasame"],
+    vegetarian: 0,
+    dishDescription: "Ever try the authentic Korean beef Bugogli?",
     cooker: "John Ahg",
+    cookerImage:"https://images.unsplash.com/photo-1543716627-839b54c40519?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTN8fGNoZWZ8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
     cookerScore: ["star", "star", "star"],
     price: "12",
     pickupDate: "29_03_2020",
@@ -20,8 +30,15 @@ export const FoodTable = [
       "https://www.dinneratthezoo.com/wp-content/uploads/2019/06/beef-bulgogi-4.jpg",
   },
   {
+    dishID: "3",
     dishName: "Matcha cookies",
+    ingredients: ["egg", "milk", "flour", "butter", "matcha"],
+    vegetarian: 1,
+    dishDescription:
+      "What can be better with a piece of Matcha Cookies to kick start of your weekend?",
+
     cooker: "Alice Julia",
+    cookerImage:"https://images.unsplash.com/photo-1534818113099-dbe2b2e800ae?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80",
     cookerScore: ["star", "star", "star", "star"],
     price: "3",
     pickupDate: "19_03_2020",
@@ -30,8 +47,14 @@ export const FoodTable = [
       "http://www.ahueats.com/wp-content/uploads/2016/03/Matcha-Cookies-2.jpg",
   },
   {
+    dishID: "4",
     dishName: "chow mein",
+    ingredients: ["noodles", "carrot", "zucchini", "spring onion", "chili"],
+    vegetarian: 1,
+    dishDescription:
+      "not the usually chow mein you find in 'Chinese restaurant'",
     cooker: "Annie Chu",
+    cookerImage:"https://images.unsplash.com/photo-1614427855823-fc9eca8a5e2e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
     cookerScore: ["star", "star"],
     price: "7",
     pickupDate: "20_03_2020",
