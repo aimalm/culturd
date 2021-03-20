bbcd0fc0cb409a87245a78e6d461cf45db303645
ismiandhyka18 <ismiandhykawaty@icloud.com>
2021-03-20 09:55:22 +0100

    Change homepage workshop style.

--- client/src/components/Workshop/AboutUsW/AboutUsW.js
+++ client/src/components/Workshop/AboutUsW/AboutUsW.js
@@ -1,20 +1,14 @@
 import React from 'react'
 import './AboutUsW.css'
 import logo from '../image/aboutus.png'
-import bgimage from '../image/bg5.png'
+
 
 
 
 function AboutUsW() {
     return (
     <>
-        <div className="container-aboutus" 
-          style={{
-          backgroundImage: `url(${bgimage})`,
-          height: "100vh",
-         
-         
-        }} >
+        <div className="container-aboutus" >
         <div className="text-aboutUS">
             <h1 className="h1-aboutus">WONDERFUL TEAM</h1><br></br>
              <h4>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. <br></br>Aenean commodo ligula eget dolor. Aenean massa.<br></br> Cum sociis natoque penatibus et magnis dis<br></br> parturient montes, nascetur ridiculus mus. 
--- client/src/components/Workshop/LandingW/LandingW.js
+++ client/src/components/Workshop/LandingW/LandingW.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import './LandingW.css'
-import logo from '../image/poto1.png'; 
 import team from '../image/aboutus.png'
-import cooking from '../image/cooking.png'
-import dance from '../image/dance.png'
-import art from '../image/art.png'
 
 
 
@@ -12,9 +8,6 @@ function LandingW() {
     return (
     <>
      <div className="container-homepage" >
-         <div className="image-logo">
-            <img className="img-class" src={logo} alt="Logo" />
-         </div>
         <div className="dummy-text">
              <h1>CULTURD WORKSHOP</h1><br></br>
             <h4>We bring our Culture to you.</h4>
@@ -24,58 +17,20 @@ function LandingW() {
              
         </div>
     </div>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="rgba(191,237,228,255) " fill-opacity="1" d="M0,256L48,266.7C96,277,192,299,288,266.7C384,235,480,149,576,101.3C672,53,768,43,864,48C960,53,1056,75,1152,101.3C1248,128,1344,160,1392,176L1440,192L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#bfede4" fill-opacity="1" d="M0,192L480,224L960,256L1440,160L1440,0L960,0L480,0L0,0Z"></path>
     </svg>
 
     <div className="container-aboutus">
         <div className="text-aboutUS">
-            <h1>WORKSHOP TEAM</h1><br></br>
+            <h1>Meet The Team</h1><br></br>
              <h4>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. <br></br>Aenean commodo ligula eget dolor. Aenean massa.<br></br> Cum sociis natoque penatibus et magnis dis<br></br> parturient montes, nascetur ridiculus mus. 
             <br></br>Donec quam felis, ultricies nec.</h4>
         </div>
         <div className="image-aboutusW">
-            <img className="img-class" src={team} alt="team"/>
+            <img className="img-team" src={team} alt="team"/>
         </div>
     </div>
-    <div className="container-ourservice">
-        <h1 className="text-ourservise">OUR WORKSHOP</h1>
-    <div className="wrapper-cooking">
-        <div className="img-cooking" >
-            <img  className="img-class" src={cooking} alt="cooking"/>
-        </div>
-        <div className="text-cooking">
-             <h2>Cooking Workshop </h2>
-             <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. <br></br>Aenean commodo ligula eget dolor. Aenean massa.<br></br> Cum sociis natoque penatibus et magnis dis<br></br> parturient montes, nascetur ridiculus mus. 
-            <br></br>Donec quam felis, ultricies nec.
-            </p>
-        </div>
-    </div>
-        <div className="wrapper-dance">
-        <div className="text-cooking">
-          <h2>Dance Workshop </h2>
-          <p>
-          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. <br></br>Aenean commodo ligula eget dolor. Aenean massa.<br></br> Cum sociis natoque penatibus et magnis dis<br></br> parturient montes, nascetur ridiculus mus. 
-            <br></br>Donec quam felis, ultricies nec.
-          </p>
-        </div>
-        <div className="img-dance">
-        <img className="img-class" src={dance} alt="dance"/>
-      </div>
-    </div>
-    <div className="wrapper-art">
-        <div className="img-art">
-            <img className="img-class" src={art}  alt="art"/>
-        </div>
-        <div className="text-cooking">
-             <h2>Art Workshop </h2>
-             <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. <br></br>Aenean commodo ligula eget dolor. Aenean massa.<br></br> Cum sociis natoque penatibus et magnis dis<br></br> parturient montes, nascetur ridiculus mus. 
-            <br></br>Donec quam felis, ultricies nec.
-            </p>
-        </div>
-    </div>
-    </div>
-
-
+    
     </>
         
     )
