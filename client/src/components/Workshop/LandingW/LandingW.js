3a92e30f411a2ebb90c3c7cc66908680b2e44b63
ismiandhyka18 <ismiandhykawaty@icloud.com>
2021-03-22 07:46:49 +0100

    Adding extrz button in homepage workshop

--- client/src/components/Workshop/LandingW/LandingW.js
+++ client/src/components/Workshop/LandingW/LandingW.js
@@ -21,13 +21,12 @@ function LandingW() {
             <Link className="landing_main-button" to="/workshop/about_us">Discover More</Link>{" "}
         </div>
     </div>
-       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#bfede4" fill-opacity="1" d="M0,192L480,224L960,256L1440,160L1440,0L960,0L480,0L0,0Z"></path>
-      </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#bfede4" fill-opacity="1" d="M0,128L720,256L1440,96L1440,0L720,0L0,0Z"></path>
+    </svg>
       <div className="container-aboutus">
         <div className="text-aboutUS">
             <h1 className="h1-aboutus">Meet The Team</h1><br></br>
-             <h4>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. <br></br>Aenean commodo ligula eget dolor. Aenean massa.<br></br> Cum sociis natoque penatibus et magnis dis<br></br> parturient montes, nascetur ridiculus mus. 
-            <br></br>Donec quam felis, ultricies nec.</h4>
+             <h4 className="h4-aboutus">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.Aenean commodo ligula eget dolor. Aenean massa.Cum sociis natoque penatibus et magnis disparturient montes, nascetur.</h4>
         </div>
         <div className="image-aboutusW">
             <img className="img-team" src={team} alt="team"/>
@@ -37,7 +36,7 @@ function LandingW() {
     </svg>
    <div className="wrapper-service__home">
     <IconContext.Provider value={{ color: '#f59d87', size: 30 }}>
-     <h1 className="h1-icon" > Why Choose Culturd Workshop ?</h1>
+     <h1 className="h1-icon" > Why Culturd Workshop?</h1>
       <div className="container-service">
           <div className='icon-home'>
               <AiFillEnvironment />
@@ -60,13 +59,16 @@ function LandingW() {
             <div className='icon-home'>
               <AiFillEnvironment />
               </div>
-            <div className="icon-home">
+              <div className="text-service">
+             <div className="icon-home">
               <h4 className="h4-icon">Everything is possible, from only one simple activity till a festival with all the trimmings. The pleasure and the togetherness.</h4>
+            </div>
             </div>
       </div>
       </IconContext.Provider>
+      <br></br><br></br><br></br>
+      <Link className="button-about" to="/workshop/about_us">Find your ideal workshop</Link>{" "}
     </div>
-   
 
    
 
