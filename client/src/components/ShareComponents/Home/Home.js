5878a753978db3552e4f3f1c28f7131f6096c9af
chevtong <chevtongwingyan@gmail.com>
2021-03-25 17:25:23 +0100

    Change homepage structure

--- client/src/components/ShareComponents/Home/Home.js
+++ client/src/components/ShareComponents/Home/Home.js
@@ -1,31 +1,45 @@
 import React from "react";
-import mainImage from "../image/test2.png";
+// import mainImage from "../image/test2.png";
 import "./Home.css";
-import { Button } from "react-bootstrap";
-import workshop from "../image/workshop.png";
+// import { Button } from "react-bootstrap";
+// import workshop from "../image/workshop.png";
 // import foodie from "../image/foodie.png";
 import { Link } from "react-router-dom";
 
 function Home() {
-
   return (
-    
-       <div className="home-wrapper">
-         <div className="home-workshop-section">
-           img here
-         </div>
-         <div className="home-food-section">
-           img here
-         </div>
-         <div className="home-workshop-text">
-          <h3>culturd workshop</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab enim, repudiandae quidem odit laborum similique velit perspiciatis aliquid inventore, impedit cum labore officiis dolorum! Fuga dicta qui consectetur distinctio vel.</p>
-         </div>
-         <div className="home-food-text">
-          <h3>culturd home food</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab enim, repudiandae quidem odit laborum similique velit perspiciatis aliquid inventore, impedit cum labore officiis dolorum! Fuga dicta qui consectetur distinctio vel.</p>
-         </div>
+    <div className="home-wrapper">
+      
+      <Link to="/workshop" className="home-workshop-section">
+       
+          <p>img here</p>
+          <h5 className="home-workshop-button">click to see more</h5>
 
+      </Link>
+      <Link to="/food" className="home-food-section">
+        
+          <p>img here</p>
+          <h5 className="home-food-button">click to see more</h5>
+        
+      </Link>
+      <Link to="/workshop" className="home-workshop-text">
+        
+          <h3>culturd workshop</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores,
+            labore.
+          </p>
+        
+      </Link>
+      <Link to="/food" className="home-food-text">
+     
+          <h3>culturd home food</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores,
+            labore.
+          </p>
+        
+      </Link>
 
       {/* <div className="container-homeMain">
         <img className="image-logo" src={mainImage} alt="Logo" />
@@ -69,9 +83,7 @@ function Home() {
         </div>
  
       </div> */}
-       </div>
-
-    
+    </div>
   );
 }
 
