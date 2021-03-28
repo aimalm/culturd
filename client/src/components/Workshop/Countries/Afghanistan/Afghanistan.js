9d74a646c795a8d4ba5d074f9b2bebd9e2e80c90
Valerie <akpotevalerie@gmail.com>
2021-03-28 20:55:24 +0200

    Add JSX accordion component

--- client/src/components/Workshop/Countries/Afghanistan/Afghanistan.js
+++ client/src/components/Workshop/Countries/Afghanistan/Afghanistan.js
@@ -2,22 +2,13 @@ import React, { useState } from 'react';
 import './Afghanistan.css';
 import { IoTicketSharp } from "react-icons/io5";
 import { BsFillLightningFill } from "react-icons/bs";
-import { FaChevronUp, FaChevronDown } from "react-icons/fa";
 import ImageCards from '../compCountries/ImageCards/ImageCards';
+import Accordeon from '../compCountries/Accordion/Accordion';
 import AfghanFood from '../../image/afghanistan.jpg';
 import { AfghanFoodInfo } from '../compCountries/ImageCards/FoodInfo';
 import { AfghanWorkshopInfo } from '../compCountries/Accordion/WorkshopInfo';
-import { IndexLinkContainer } from 'react-router-bootstrap';
 
 function Afghanistan() {
-    const [clicked, setClicked] = useState(false);
-    const toggleAngle = index => {
-        if (clicked === index) {
-            return setClicked(null);
-        }
-
-        setClicked(index);
-    }
     return (
         <div className="country-container">
             <div className="workshop-info">
@@ -39,21 +30,7 @@ function Afghanistan() {
                         foodTit_3={AfghanFoodInfo[2].title}
                     />
                     <div className="accordeon-container">
-                        {AfghanWorkshopInfo.map((item, index) => {
-                            return (
-                                <div className="info-section">
-                                    <div className="section-wrap" onClick={() => toggleAngle(index)} key={index}>
-                                        <h5>{item.title}</h5>
-                                        <span>{clicked === index? <FaChevronUp /> : <FaChevronDown />}</span>
-                                    </div>
-                                    {clicked === index? (
-                                        <div className="dropDown">
-                                            <p>{item.content}</p>
-                                        </div>  
-                                    ) : null}
-                                </div>
-                            )
-                        })}
+                        <Accordeon content={AfghanWorkshopInfo}/>
                     </div>
                 </div>
                 <div className="container-1">
--- client/src/components/Workshop/Countries/compCountries/Accordion/Accordion.js
+++ client/src/components/Workshop/Countries/compCountries/Accordion/Accordion.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Accordion.css';
+import { FaChevronUp, FaChevronDown } from "react-icons/fa";
 
-function Accordion() {
+
+function Accordion(accordeonProp) {
+    const [clicked, setClicked] = useState(false);
+    const toggleAngle = index => {
+        if (clicked === index) {
+            return setClicked(null);
+        }
+
+        setClicked(index);
+    }
     return (
         <div>
-            
+            {accordeonProp.content.map((item, index) => {
+                return (
+                    <div className="info-section">
+                        <div className="section-wrap" onClick={() => toggleAngle(index)} key={index}>
+                            <h5>{item.title}</h5>
+                            <span>{clicked === index? <FaChevronUp /> : <FaChevronDown />}</span>
+                        </div>
+                        {clicked === index? (
+                            <div className="dropDown">
+                                <p>{item.content}</p>
+                            </div>  
+                        ) : null}
+                    </div>
+                )
+            })}
         </div>
     );
 }
