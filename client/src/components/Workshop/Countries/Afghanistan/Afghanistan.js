a00a59d237b807a559760bc0d96766be53100dc3
Valerie <akpotevalerie@gmail.com>
2021-03-28 19:46:25 +0200

    Add dummy data for accordeon

--- client/src/components/Workshop/Countries/Afghanistan/Afghanistan.js
+++ client/src/components/Workshop/Countries/Afghanistan/Afghanistan.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Afghanistan.css';
 import { IoTicketSharp } from "react-icons/io5";
 import { BsFillLightningFill } from "react-icons/bs";
+import { FaAngleDown } from "react-icons/fa";
+import { FaAngleUp } from "react-icons/fa";
 import ImageCards from '../compCountries/ImageCards/ImageCards';
 import AfghanFood from '../../image/afghanistan.jpg';
-import {AfghanFoodInfo} from '../compCountries/ImageCards/FoodInfo';
+import { AfghanFoodInfo } from '../compCountries/ImageCards/FoodInfo';
+import { AfghanWorkshopInfo } from '../compCountries/Accordion/WorkshopInfo';
 
 function Afghanistan() {
     return (
@@ -27,6 +30,9 @@ function Afghanistan() {
                         foodDes_3={AfghanFoodInfo[2].description}
                         foodTit_3={AfghanFoodInfo[2].title}
                     />
+                    <div className="accordeon-container">
+                        
+                    </div>
                 </div>
                 <div className="container-1">
                     
