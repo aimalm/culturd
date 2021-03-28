e3d8bd82f4a5a60dd81cb06e065456a7f7cac0dc
Valerie <akpotevalerie@gmail.com>
2021-03-28 13:17:26 +0200

    Create functional component for image cards

---
+++ client/src/components/Workshop/Countries/compCountries/ImageCards/ImageCards.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ImageCards.css';
+
+function ImageCards() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default ImageCards;
