b6b9f22999321e5ff762eedb24442ef348008270
Valerie <akpotevalerie@gmail.com>
2021-03-28 13:14:10 +0200

    Add JSX and styling for workshop information

--- client/src/components/Workshop/Countries/Congo/Congo.js
+++ client/src/components/Workshop/Countries/Congo/Congo.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './Congo.css';
+import { IoTicketSharp } from "react-icons/io5";
+import { BsFillLightningFill } from "react-icons/bs";
 
 function Congo() {
     return (
-        <div>
-            
+        <div className="country-container">
+            <div className="workshop-info">
+                <h2>Congolese Cooking Workshop</h2>
+                <div className="ticket-info">
+                    <p><span className="info-icon"><IoTicketSharp /></span> E-Ticket</p>
+                    <p><span className="info-icon"><BsFillLightningFill /></span> Instant Confirmation</p>
+                </div>
+            </div>
         </div>
     );
 }
--- client/src/components/Workshop/Countries/Indonesia/Indonesia.js
+++ client/src/components/Workshop/Countries/Indonesia/Indonesia.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './Indonesia.css';
+import { IoTicketSharp } from "react-icons/io5";
+import { BsFillLightningFill } from "react-icons/bs";
 
 function Indonesia() {
     return (
-        <div>
-            
+        <div className="country-container">
+            <div className="workshop-info">
+                <h2>Indonesian Cooking Workshop</h2>
+                <div className="ticket-info">
+                    <p><span className="info-icon"><IoTicketSharp /></span> E-Ticket</p>
+                    <p><span className="info-icon"><BsFillLightningFill /></span> Instant Confirmation</p>
+                </div>
+            </div>
         </div>
     );
 }
