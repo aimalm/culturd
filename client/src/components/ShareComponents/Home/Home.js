8b74ba9622a6b66f34d57d1165a3766e1ad9ad7d
ismiandhyka18 <ismiandhykawaty@icloud.com>
2021-03-28 22:16:41 +0200

    styling homepage workshop and 4 country grid

--- client/src/components/ShareComponents/Home/Home.js
+++ client/src/components/ShareComponents/Home/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Home.css";
 import { Link } from "react-router-dom";
-import workshop from '../image/WORKSHOP.png'
+import workshop from '../image/workshop.png'
 import food from '../image/food.png'
 
 
