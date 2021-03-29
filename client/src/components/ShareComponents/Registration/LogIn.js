e66f36ecdfb589a00d4a72aa26441daf505eb30c
Valerie <akpotevalerie@gmail.com>
2021-03-29 19:13:34 +0200

    Add JSX and styling sign up page

--- client/src/components/ShareComponents/Registration/LogIn.js
+++ client/src/components/ShareComponents/Registration/LogIn.js
@@ -8,25 +8,28 @@ function Login() {
     return (
         <div className="login-container">
             <div className="reg-button-container">
-            <button className="reg-dropdown-button" onClick={()=>setShowLogin(!showLogin)}>
-            Register <AiFillCaretDown />
-            </button>
-            {showLogin ? (
-            <div className="reg-dropdown-container">
-                <Link to="/login" className="reg-login-link">
-                <p>Log In</p>
-                </Link>
-                <Link to="/signup" className="reg-signup-link">
-                <p>Sign Up</p>
-                </Link>
-                <Link to="/contact_us" className="reg-contact-us-link">
-                <p>Contact Us</p>
-                </Link>
+                <button className="reg-dropdown-button" onClick={()=>setShowLogin(!showLogin)}>
+                Register <AiFillCaretDown />
+                </button>
+                {showLogin ? (
+                <div className="reg-dropdown-container">
+                    <Link to="/" className="reg-login-link">
+                    <p>Home</p>
+                    </Link>
+                    <Link to="/login" className="reg-login-link">
+                    <p>Log In</p>
+                    </Link>
+                    <Link to="/signup" className="reg-signup-link">
+                    <p>Sign Up</p>
+                    </Link>
+                    <Link to="/" className="reg-contact-us-link">
+                    <p>Contact Us</p>
+                    </Link>
+                </div>
+                ) : (
+                ""
+                )}
             </div>
-            ) : (
-            ""
-            )}
-        </div>
             <div className="log-form-container">
                 <div className="form-container-1">
                     <h2>New User?</h2>
--- client/src/components/ShareComponents/Registration/SignUp.js
+++ client/src/components/ShareComponents/Registration/SignUp.js
@@ -1,10 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Registration.css';
+import { Link } from "react-router-dom";
+import { AiFillCaretDown } from "react-icons/ai";
 
 function SignUp() {
+    const [showLogin, setShowLogin] = useState(false);
     return (
-        <div>
-            
+        <div className="signup-container">
+            <div className="reg-button-container">
+                <button className="reg-dropdown-button" onClick={()=>setShowLogin(!showLogin)}>
+                Register <AiFillCaretDown />
+                </button>
+                {showLogin ? (
+                <div className="reg-dropdown-container">
+                    <Link to="/" className="reg-login-link">
+                    <p>Home</p>
+                    </Link>
+                    <Link to="/login" className="reg-login-link">
+                    <p>Log In</p>
+                    </Link>
+                    <Link to="/signup" className="reg-signup-link">
+                    <p>Sign Up</p>
+                    </Link>
+                    <Link to="/" className="reg-contact-us-link">
+                    <p>Contact Us</p>
+                    </Link>
+                </div>
+                ) : (
+                ""
+                )}
+            </div>
+            <div className="sign-form-container">
+                <div className="form-container-1">
+                    <h2>Already have an account?</h2>
+                    <p><Link to="/login" className="reg-link">Log in</Link> to get access to all features of the application</p>
+                </div>
+                <div className="form-container-2">
+                    <form>
+                        <h4>Sign up</h4>
+                        <div className="form-group">
+                            <label htmlFor="firstName">First Name</label><br/>
+                            <input type="text" name="firstName" id="firstName" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="lastName">Last Name</label><br/>
+                            <input type="text" name="lastName" id="lastName" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="email">Email</label><br/>
+                            <input type="email" name="email" id="sign-email" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label><br/>
+                            <input type="email" name="password" id="sign-pass" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="confirmPass">Confirm Password</label><br/>
+                            <input type="password" name="confirmPass" id="confirmPass" />
+                        </div>
+                        <button type="submit" className="submit-btn">Signup</button>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 }
