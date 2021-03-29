d134f0cca026905df73e1fb4926f366e13e6ab1a
chevtong <chevtongwingyan@gmail.com>
2021-03-29 20:27:09 +0200

    Build shoppingCartF responsive

--- client/src/components/Food/ProductListF/ProductListF.js
+++ client/src/components/Food/ProductListF/ProductListF.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./ProductListF.css";
 import { FoodTable } from "../Template";
 import { Link } from "react-router-dom";
@@ -7,66 +7,106 @@ import StarIcon from "@material-ui/icons/Star";
 
 function ProductListF({ setSelectedDish }) {
   const [filteredDish, setFilteredDish] = useState(FoodTable);
+  const [addAnimationClass, setAddAnimationClass] = useState(false);
 
   const selectHandler = (dishID) => {
     setSelectedDish(FoodTable.filter((item) => item.id === dishID));
   };
 
   const filterHandler = (e) => {
-  
     switch (e.target.value) {
       case "main":
         setFilteredDish(FoodTable.filter((food) => food.category === "main"));
-        
+        toggleClass()
         break;
 
       case "side":
         setFilteredDish(FoodTable.filter((food) => food.category === "side"));
+        toggleClass()
         break;
 
       case "dessert":
-        setFilteredDish(FoodTable.filter((food) => food.category === "dessert"));
+        setFilteredDish(
+          FoodTable.filter((food) => food.category === "dessert")
+        );
+        toggleClass()
         break;
 
       case "veg":
         setFilteredDish(FoodTable.filter((food) => food.vegetarian));
+        toggleClass()
         break;
 
       default:
         setFilteredDish(FoodTable);
         break;
     }
+
   };
 
+  const toggleClass = () =>{
+    setAddAnimationClass(true)
+
+    setTimeout(() => {
+      setAddAnimationClass(false)
+    }, 1500);
+  }
+
   return (
-    <div>
+    <div className="search-wrapper">
       <div className="search-button-container">
-        
-        <button className="search-filter-button all-button" value="all" onClick={(e) => filterHandler(e)}>
+        <button
+          className="search-filter-button all-button"
+          value="all"
+          onClick={(e) => filterHandler(e)}
+        >
           See All
         </button>
-     
-     
-        <button className="search-filter-button" value="main" onClick={(e) => filterHandler(e)}>
+
+        <button
+          className="search-filter-button"
+          value="main"
+          onClick={(e) => filterHandler(e)}
+        >
           main dish
         </button>
 
-        <button className="search-filter-button" value="side" onClick={(e) => filterHandler(e)}>
+        <button
+          className="search-filter-button"
+          value="side"
+          onClick={(e) => filterHandler(e)}
+        >
           side dish
         </button>
-        <button className="search-filter-button" value="dessert" onClick={(e) => filterHandler(e)}>
+        <button
+          className="search-filter-button"
+          value="dessert"
+          onClick={(e) => filterHandler(e)}
+        >
           desserts
         </button>
-        <button className="search-filter-button" value="veg" onClick={(e) => filterHandler(e)}>
+        <button
+          className="search-filter-button"
+          value="veg"
+          onClick={(e) => filterHandler(e)}
+        >
           Vegetarian
         </button>
       </div>
 
-      <div className="search-list">
+      <div className={addAnimationClass?"random-container list-container": "random-container"}>
+     
+
         {filteredDish.map((dish) => (
-          <Link key={dish.id} to={"/food/products/" + dish.id}>
-            <div className="food-card" onClick={() => selectHandler(dish.id)}>
-              <img className="product-image" src={dish.imageUrl} alt="" />
+          <div className="food-card">
+            <Link
+              key={dish.id}
+              to={"/food/products/" + dish.id}
+              onClick={() => selectHandler(dish.id)}
+            >
+              <div className="food-card-img">
+                <img className="product-image" src={dish.imageUrl} alt="" />
+              </div>
               <div className="food-info">
                 <div className="more-button-wrapper">
                   <h4 className="dish-name">{dish.dishName}</h4>
@@ -74,16 +114,18 @@ function ProductListF({ setSelectedDish }) {
                     <AddCircleSharpIcon className="more-icon" />
                   </button>
                 </div>
-                <p className="cook-score">
+                <p className="cook-info">
                   {dish.cooker}
+                  <br></br>
                   {dish.cookerScore.map((star, index) => (
                     <StarIcon key={index} className="star" />
                   ))}
                 </p>
               </div>
-            </div>
-          </Link>
+            </Link>
+          </div>
         ))}
+
       </div>
     </div>
   );
--- client/src/components/Food/ShoppingCartF/ShoppingCartF.js
+++ client/src/components/Food/ShoppingCartF/ShoppingCartF.js
@@ -2,28 +2,26 @@ import React from "react";
 import "./ShoppingCartF.css";
 import { IoMdArrowBack } from "react-icons/io";
 import EuroSymbolIcon from "@material-ui/icons/EuroSymbol";
-import {BsPlus} from 'react-icons/bs'
-import {BiMinus} from 'react-icons/bi'
+import { BsPlus } from "react-icons/bs";
+import { BiMinus } from "react-icons/bi";
 
 function ShoppingCartF({ shoppingCart, setShoppingCart }) {
-  
-  const subTotal = shoppingCart.reduce((a, b) => (a + b.price)*b.quantity, 0);
+  const subTotal = shoppingCart.reduce((a, b) => (a + b.price) * b.quantity, 0);
 
   const deleteFromCart = (id) => {
     let newShoppingCart = shoppingCart.filter((food) => food.id !== id);
     setShoppingCart(newShoppingCart);
   };
-  
-  const changeQuanityHandler = (e, id) =>{
-  
+
+  const changeQuanityHandler = (e, id) => {
     switch (e.currentTarget.value) {
       case "plus":
         const plusArray = [...shoppingCart];
         shoppingCart.map((item) => {
           if (item.id === id) {
-            item.quantity ++
+            item.quantity++;
           }
-        })
+        });
         setShoppingCart(plusArray);
         break;
 
@@ -32,34 +30,36 @@ function ShoppingCartF({ shoppingCart, setShoppingCart }) {
 
         shoppingCart.map((item) => {
           if (item.id === id && item.quantity > 1) {
-            item.quantity --
+            item.quantity--;
             setShoppingCart(minusArray);
-          } else  {
-            deleteFromCart(id)
-          } 
-        })
+          } else {
+            deleteFromCart(id);
+          }
+        });
         break;
 
       default:
-        setShoppingCart(shoppingCart)
+        setShoppingCart(shoppingCart);
         break;
-  }
-}
+    }
+  };
 
   return (
     <div className="shopping-cart-wrapper">
       <a href="/food/product_list" className="back-button">
         <IoMdArrowBack className="back-button-icon" />
       </a>
-      <h1 className="shopping-cart-title">My Order</h1>
+      <h3 className="shopping-cart-title">My Order</h3>
 
       {shoppingCart.length === 0 ? (
-        <h1>Your cart is empty</h1>
+        <h3 className="shopping-cart-title">
+          Shopping cart is empty, <br></br>search for the your next HOME COOK
+          FOOD!
+        </h3>
       ) : (
         <div>
           {shoppingCart.map((food) => (
             <div key={food.id} className="shopping-cart-item">
-              
               <img className="cart-img" src={food.imageUrl} alt="" />
               <div className="cart-food-info">
                 <h4>{food.dishName}</h4>
@@ -73,26 +73,50 @@ function ShoppingCartF({ shoppingCart, setShoppingCart }) {
               </div>
 
               <div className="cart-quantity">
-                <button className="cart-plus-button" onClick={(e)=>changeQuanityHandler(e, food.id)} value="plus"><BsPlus/>
+                <button
+                  className="cart-plus-button"
+                  onClick={(e) => changeQuanityHandler(e, food.id)}
+                  value="plus"
+                >
+                  <BsPlus />
                 </button>
-                <input type="text" name="name" value={food.quantity} className="cart-quantity-input" disabled/>
-                <button className="cart-minus-button" onClick={(e)=>changeQuanityHandler(e,  food.id)} value="minus"><BiMinus/>
+                <input
+                  type="text"
+                  name="name"
+                  value={food.quantity}
+                  className="cart-quantity-input"
+                  disabled
+                />
+                <button
+                  className="cart-minus-button"
+                  onClick={(e) => changeQuanityHandler(e, food.id)}
+                  value="minus"
+                >
+                  <BiMinus />
                 </button>
-         
               </div>
-              <button  className="cart-delete-button" onClick={() => deleteFromCart(food.id)}>Delete</button>
+              <button
+                className="cart-delete-button"
+                onClick={() => deleteFromCart(food.id)}
+              >
+                Delete
+              </button>
             </div>
           ))}
 
           <div className="cart-breakline"></div>
-          <div className="shopping-cart-item">
-              <div></div>
-           
-              <h3>Subtotal</h3>
-
-            <h3><EuroSymbolIcon /> {subTotal}</h3>
+          <div className="cart-subtotal-container">
             
-            <button className="home-food-link cart-checkout-button">Check Out</button>
+
+            <h5 className="cart-subtotal-title">Subtotal</h5>
+
+            <h5 className="cart-subtotal-price">
+              <EuroSymbolIcon /> {subTotal}
+            </h5>
+
+            <button className="home-food-link cart-checkout-button">
+              Check Out
+            </button>
           </div>
         </div>
       )}
