65694d7cf59771998a23b1e27ddb1e77281a55df
Valerie <akpotevalerie@gmail.com>
2021-04-05 08:41:56 +0200

    Add content

--- client/src/components/Workshop/AboutUsW/AboutUsW.js
+++ client/src/components/Workshop/AboutUsW/AboutUsW.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './AboutUs.css';
-import { concept, mission, vision, people, about } from './AboutUsWcontent';
+import { concept, mission, vision, people } from './AboutUsWcontent';
 
 function AboutUsW() {
     return (
