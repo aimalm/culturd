dd0057e631b3b3ad92b1337f3c809f701b2191c6
Valerie <akpotevalerie@gmail.com>
2021-04-05 08:26:14 +0200

    Add padding at the bottem

--- client/src/components/Workshop/AboutUsW/AboutUsW.js
+++ client/src/components/Workshop/AboutUsW/AboutUsW.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import './AboutUsW.css';
-import { concept, mission, vision, people } from './AboutUsWcontent';
+import './AboutUs.css';
+import { concept, mission, vision, people, about } from './AboutUsWcontent';
 
 function AboutUsW() {
     return (
@@ -14,15 +14,15 @@ function AboutUsW() {
                 </div>
             </div>
             <div className="trilogy">
-                <div className="mission">
+                <div className="mis-vis-p">
                     <h3>mission</h3>
                     <p>{mission}</p>
                 </div>
-                <div className="vision">
+                <div className="mis-vis-p">
                     <h3>vision</h3>
                     <p>{vision}</p>
                 </div>
-                <div className="people">
+                <div className="mis-vis-p">
                     <h3>people</h3>
                     <p>{people}</p>
                 </div>
