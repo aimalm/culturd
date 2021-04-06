69cad2b9bd972bc5d2e8ef3b493d7262fc6b5954
chevtong <chevtongwingyan@gmail.com>
2021-04-06 13:20:12 +0200

    WIP - fixing conflict

--- client/src/components/ShareComponents/Registration/LogIn.js
+++ client/src/components/ShareComponents/Registration/LogIn.js
@@ -2,7 +2,6 @@ import React, { useRef, useState, useEffect } from "react";
 import "./Registration.css";
 import { Link } from "react-router-dom";
 import NavLogSign from "../NavLogSign/NavLogSign";
-import home from '../image/home.png';
 import axios from "axios";
 
 function Login() {
@@ -13,7 +12,6 @@ function Login() {
   useEffect(() => {
     setSignInError();
   }, []);
-  
 
   function logIn(e) {
     e.preventDefault();
@@ -35,9 +33,6 @@ function Login() {
       }
       return req;
     });
-    // //Get all for food
-    // const getUser = async () => {
-    //     const response = await API.get(`/user/${userEmail}`).catch((err) => console.log(err));
 
     //Get all for user
     const getUser = async () => {
@@ -68,43 +63,59 @@ function Login() {
       });
   }
 
-
-  
-    return (
-        <div>
-            <div className="home-arrow">
-                <Link to="/">
-                    <img src={home} className="arrow" alt=""/>
+  return (
+    <div className="login-container">
+      <NavLogSign />
+      <div className="log-form-container">
+        <div className="form-container-1">
+          <h2 className="login-h2">New User ?</h2>
+          <p className="p-login">
+            <Link to="/signup" className="reg-link">
+              Sign up
+            </Link>{" "}
+            to get access to all features of the application
+          </p>
+        </div>
+        <div className="form-container-2">
+          <form>
+            <h1>Log in</h1>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <br />
+              <input
+                ref={email}
+                type="email"
+                name="email"
+                id="log-email"
+                className="input-fields"
+                required
+              />
+              <p className="error-msg"></p>
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <br />
+              <input
+                ref={password}
+                type="password"
+                name="password"
+                id="log-pass"
+                className="input-fields"
+                required
+              />
+              <p className="error-msg">{SignInError}</p>
+              <p>
+                <Link to="/" id="password-link">
+                  Forgot password?
                 </Link>
+              </p>
             </div>
-            <div className="login-container">
-                <div className="log-form-container">
-                    <div className="form-container-1">
-                        <h2 className="login-h2">New User?</h2>
-                        <p className="p-login"><Link to="/signup" className="reg-link">Sign up</Link> to get access to all features of the application</p>
-                    </div>
-                    <div className="form-container-2">
-                        <form>
-                            <h1>Log in</h1>
-                            <div className="form-group">
-                                <label htmlFor="email">Email</label><br/>
-                                <input ref = {email} type="email" name="email" id="log-email" className="input-fields" required/>
-                                <p className="error-msg"></p>
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="password">Password</label><br/>
-                                <input ref = {password} type="password" name="password" id="log-pass" className="input-fields" required/>
-                                <p className="error-msg">{SignInError}</p>
-                                <p><Link to="/" id="password-link">Forgot password?</Link></p>
-                            </div>
-                            <button type="submit"className="submit-btn" onClick={logIn}>Login</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            
-        
-     
+            <button type="submit" className="submit-btn" onClick={logIn}>
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
