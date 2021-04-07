6c48de4b2ddd66db90d16d93bbf6f95555db615d
Valerie <akpotevalerie@gmail.com>
2021-04-08 00:19:08 +0200

    Replace dummy content by real content

--- client/src/components/Food/AboutUsF/AboutUsFcontent.js
+++ client/src/components/Food/AboutUsF/AboutUsFcontent.js
@@ -1,17 +1,17 @@
-const conceptF = `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-Donec gravida tincidunt euismod. Mauris lacus mi, vulputate eget sem
-vitae, imperdiet consectetur urna. In ante sapien, mattis eu nisi ut,
-malesuada fringilla massa. 
+const conceptF = `CULTURD FOOD aims to connect food lovers and passionate home cooks
+by providing a platform where home cooks can sell their meals, 
+authentic to their home country. Foodies can get access to food they
+will never see on a restaurant menu but are staple to one's culture.    
 
-Ut pulvinar, quam quis interdum convallis,
-sem dolor ullamcorper lacus, vel tristique felis nunc in risus.
-Duis ex dolor, commodo vitae metus ac, egestas tincidunt turpis.
+If you enjoy the distinguish taste of home cooked food and want to discover
+authentic meals, then we just might be what you're looking for.
+Home cooks eager to share their culture are equally welcomed with open arms.
 
-Morbi leo quam, ornare molestie pellentesque interdum, euismod ut magna.
-Aliquam tincidunt orci vulputate interdum varius.`
+Expand your taste palate, share your food and connect with others.
+CULTURD FOOD is here to make that possible.`
 
-const missionF = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Donec gravida tincidunt euismod.'
-const visionF = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Donec gravida tincidunt euismod.'
-const peopleF = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Donec gravida tincidunt euismod.'
+const missionF = 'Provide foodies access to home-cooked, authentic foreign meals prepared by hobby cooks.'
+const visionF = 'Become the go-to platform for people who enjoy home-cooked meals and hobby cooks.'
+const peopleF = 'Passionate about food and culture; a dynamic team who aims to transfer this passion.'
 
 export {conceptF, missionF, visionF, peopleF};
