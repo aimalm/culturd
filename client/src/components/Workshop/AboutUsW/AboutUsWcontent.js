384c67cd177869d4c0cbc1071c248e262b86b3c4
Valerie <akpotevalerie@gmail.com>
2021-04-08 01:23:12 +0200

    Replace workshop dummy content

--- client/src/components/Workshop/AboutUsW/AboutUsWcontent.js
+++ client/src/components/Workshop/AboutUsW/AboutUsWcontent.js
@@ -1,17 +1,16 @@
-const conceptW = `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-Donec gravida tincidunt euismod. Mauris lacus mi, vulputate eget sem
-vitae, imperdiet consectetur urna. In ante sapien, mattis eu nisi ut,
-malesuada fringilla massa. 
+const conceptW = `CULTURD WORKSHOP organizes workshops centered around a specific country
+and their culture. Workshop participants will get the chance to 
+experience the food, the dance and the art of this country all in one workshop.
+The workshops are given by people eager to share their culture and knowledge.
 
-Ut pulvinar, quam quis interdum convallis,
-sem dolor ullamcorper lacus, vel tristique felis nunc in risus.
-Duis ex dolor, commodo vitae metus ac, egestas tincidunt turpis.
+Are you interested in getting to know other cultures, get your hands dirty
+and meet new people? Then our workshops might just be what you are looking for.
 
-Morbi leo quam, ornare molestie pellentesque interdum, euismod ut magna.
-Aliquam tincidunt orci vulputate interdum varius.`
+Enrich your mind, feed your knowledge and connect with others.
+CULTURD WORKSHOP aims to give you this opportunity.`
 
-const missionW = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Donec gravida tincidunt euismod.'
-const visionW = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Donec gravida tincidunt euismod.'
-const peopleW = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Donec gravida tincidunt euismod.'
+const missionW = 'To bridge the gap between different cultures, to incite conversation and understanding.'
+const visionW = 'To provide more cultural experiences in the near future and organize unique workshops all over Belgium.'
+const peopleW = 'Born organizers; passionate team eager to connect people from different cultures.'
 
 export {conceptW, missionW, visionW, peopleW};
