cbf40df6318029b94d6201761115150dad951b2a
ismiandhyka18 <ismiandhykawaty@icloud.com>
2021-04-08 13:47:01 +0200

    Change the homepage workshop style

--- client/src/components/Workshop/LandingW/SectionGrid.js
+++ client/src/components/Workshop/LandingW/SectionGrid.js
@@ -3,26 +3,24 @@ import React from 'react'
 function SectionGrid() {
     return (
         <div>
-           <h1 className="title-home">How it works </h1>
-
+           <h1 className="h1-icon">Why Culturd Workshop ? </h1>
            <div className="container-work">
-          
             <button className="box-work">
-            <i class="fa fa-copy fa-2x"></i>
-              <h2 className="text-uppercase">Choose venue</h2>
-              <p>A wide variety of activities to stimulate variety workshop, such as arts , cooking and Dancing.</p>
+            <i class="far fa-lightbulb fa-3x"></i>
+              <p>Inspiration With more Culture original activities we have something suitable for everyone.</p>
             </button>
             <button className="box-work">
-            <i class="fa fa-users fa-2x"></i>
-              <h2 className="text-uppercase">Choose menu</h2>
-              <p>Our experienced Workshop encourage people to Learn about our Culture,brings togetherness and creatively.</p>
+            <i class="fas fa-laptop-house fa-3x"></i>
+              <p>Save time Easily find the Workshop fit to your needs. Use contact us for personal advice.</p>
             </button>
             <button className="box-work">
-            <i class="fas fa-mug-hot fa-2x"></i>
-              <h2 className="text-uppercase">Fun cooking</h2>
-              <p>Organic food, variety food, and alot of igredient delivered cooking together brings togetherness.</p>
+            <i class="far fa-smile-wink fa-3x"></i>
+              <p>We only cooperate with professional screened partners and we employ the same sharp prices as them.</p>
             </button>
-            
+            <button className="box-work">
+            <i class="fas fa-cocktail fa-3x"></i>
+              <p>Everything is possible, in our Activities The pleasure and the togetherness always be our number one priority.</p>
+            </button>
           
         </div>
 
