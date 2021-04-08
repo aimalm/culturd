b5930a158c662944e31c987bf10ac4c1a6d205f1
Valerie <akpotevalerie@gmail.com>
2021-04-08 20:02:45 +0200

    Adjust about us workshop content

--- client/src/components/Workshop/AboutUsW/AboutUsWcontent.js
+++ client/src/components/Workshop/AboutUsW/AboutUsWcontent.js
@@ -1,10 +1,10 @@
-const conceptW = `CULTURD WORKSHOP organizes workshops centered around a specific country
+const conceptW = `CULTURD WORKSHOP organizes workshops centered around four countries
 and their culture. Workshop participants will get the chance to 
-experience the food, the dance and the art of this country all in one workshop.
+experience the food, the dance and the art of these countries all in one workshop.
 The workshops are given by people eager to share their culture and knowledge.
 
-Are you interested in getting to know other cultures, get your hands dirty
-and meet new people? Then our workshops might just be what you are looking for.
+Are you interested in getting to know other cultures, getting your hands dirty
+and meeting new people? Then our workshops might just be what you are looking for.
 
 Enrich your mind, feed your knowledge and connect with others.
 CULTURD WORKSHOP aims to give you this opportunity.`
