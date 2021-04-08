8309d73d5acaa478e67dab4312c86d6a81e510d8
ismiandhyka18 <ismiandhykawaty@icloud.com>
2021-04-08 16:56:41 +0200

    change the classname

--- client/src/components/Workshop/LandingW/SectionGrid.js
+++ client/src/components/Workshop/LandingW/SectionGrid.js
@@ -6,19 +6,19 @@ function SectionGrid() {
            <h1 className="h1-icon">Why Culturd Workshop ? </h1>
            <div className="container-work">
             <button className="box-work">
-            <i class="far fa-lightbulb fa-3x"></i>
+            <i className="far fa-lightbulb fa-3x"></i>
               <p>Inspiration With more Culture original activities we have something suitable for everyone.</p>
             </button>
             <button className="box-work">
-            <i class="fas fa-laptop-house fa-3x"></i>
+            <i className="fas fa-laptop-house fa-3x"></i>
               <p>Save time Easily find the Workshop fit to your needs. Use contact us for personal advice.</p>
             </button>
             <button className="box-work">
-            <i class="far fa-smile-wink fa-3x"></i>
+            <i className="far fa-smile-wink fa-3x"></i>
               <p>We only cooperate with professional screened partners and we employ the same sharp prices as them.</p>
             </button>
             <button className="box-work">
-            <i class="fas fa-cocktail fa-3x"></i>
+            <i className="fas fa-cocktail fa-3x"></i>
               <p>Everything is possible, in our Activities The pleasure and the togetherness always be our number one priority.</p>
             </button>
           
