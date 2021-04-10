33e1d93f929d7fff26f48dfc9c078d0f2e8fc8ec
Aimal Maarij <aimal.maarij2020@gmail.com>
2021-04-10 20:53:10 +0200

    add env and gitignore files to the backed

--- server/index.js
+++ server/index.js
@@ -4,12 +4,15 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+
 
 import Routes from './routes/routes.js';
 
 
 const app = express();
-
+dotenv.config();
 app.use(express.static('./public'));
 app.use('/uploads', express.static('uploads'));
 
@@ -20,15 +23,15 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-const key = "Em3Wi5va8is15";
+const key = process.env.KEY;
 
 app.use(`/culturd_api/${key}`, Routes);
 
 
-const CONNECTION_URL = "mongodb+srv://cuturd:culturd123@cluster0.adry7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+//const CONNECTION_URL = "mongodb+srv://cuturd:culturd123@cluster0.adry7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
